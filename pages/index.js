7a5d7499de8731a5118985ed1696bf2abb85568f
christielau <christiephlau+login@gmail.com>
2025-04-10 17:36:37 +0100

    Update index.js to include portfolio link

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Christie Lau | Designer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hello! I'm Christie, a designer based in London." />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+           This space is under renovation. To see my portfolio, go to <code>https://christie-lau.webflow.io/</code>
+          <a href"https://christie-lau.webflow.io/">Explore Christie's portfolio </a>
         </p>
       </main>
 
